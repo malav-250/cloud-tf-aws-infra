a4ed6e1c4bde2fd61c151de49dc66a0b5941e963
Malav Gajera <78475119+malav-250@users.noreply.github.com>
2025-10-07 13:35:43 -0400

    added the inital normal setup of vpc

---
+++ variables.tf
@@ -0,0 +1,49 @@
+# Region and availability zones
+variable "aws_region" {
+  description = "AWS region where resources will be created"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "vpc_name" {
+  description = "Name of the VPC"
+  type        = string
+}
+
+variable "environment" {
+  description = "Environment name (dev, staging, prod)"
+  type        = string
+  default     = "dev"
+}
+
+# VPC CIDR block
+variable "vpc_cidr" {
+  description = "CIDR block for VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+# Subnet configuration
+variable "public_subnet_cidrs" {
+  description = "CIDR blocks for public subnets"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+}
+
+variable "private_subnet_cidrs" {
+  description = "CIDR blocks for private subnets"
+  type        = list(string)
+  default     = ["10.0.11.0/24", "10.0.12.0/24", "10.0.13.0/24"]
+}
+
+# Number of AZs to use
+variable "az_count" {
+  description = "Number of availability zones to use"
+  type        = number
+  default     = 3
+  
+  validation {
+    condition     = var.az_count >= 2 && var.az_count <= 6
+    error_message = "AZ count must be between 2 and 6"
+  }
+}
